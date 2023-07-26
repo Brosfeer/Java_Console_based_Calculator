41dce57fb4361ef8ac3198222294f88ba291afa1
Sharaf <alshykhshrfaldyn100@gmail.com>
2023-07-26 18:05:41 +0300

    first commit

---
+++ src/e_project_of_aptech/E_Project_of_Aptech.java
@@ -0,0 +1,62 @@
+
+package e_project_of_aptech;
+
+
+public class E_Project_of_Aptech {
+ public static double add(double no1,double no2){
+        return (no1+no2);
+    }
+    public static double sub(double no1,double no2){
+        return (no1-no2);
+    }
+    public static double mul(double no1,double no2){
+        return (no1/no2);
+    }
+    public static double div(double no1,double no2){
+    return (no1/no2);
+    }
+    public static double pow(double no1,double no2){
+        return (Math.pow(no2, no2));
+    }
+    public static double sqr(double no1){
+        return (Math.sqrt(no1));
+    }
+    public static double cube(double no1){
+        return (Math.cbrt(no1));
+    }
+    public static double sqrRoot(double no1){
+        return (Math.sqrt(no1));
+    }
+    
+    public static void main(String[] args) {
+        System.out.println("Choose Number to Perform  a Function -------------  or Zero To Exit ");
+        System.out.println(" 1 --> Addition ");
+        System.out.println(" 2 --> Subtraction ");
+        System.out.println(" 3 --> Multiplication ");
+        System.out.println(" 4 --> Division ");
+        System.out.println(" 5 --> Power ");
+        System.out.println(" 6 --> Square ");
+        System.out.println(" 7 --> Cube ");
+        System.out.println(" 8 --> Square root ");
+        System.out.println(" 9 --> Round, ");
+        System.out.println(" 10 --> Ceiling, ");
+        System.out.println(" 11 --> Floor, ");
+        System.out.println(" 12 --> Min Value ");
+        System.out.println(" 13 --> Max Value ");
+        System.out.println(" 14 --> Sin ");
+        System.out.println(" 15 --> Cos ");
+        System.out.println(" 16 --> Asin ");
+        System.out.println(" 17 --> Acos ");
+        System.out.println(" 18 --> Atan ");
+        System.out.println(" 19 --> Exponential ");
+        System.out.println(" 20 --> Palindrome, ");
+        System.out.println(" 21 --> Armstrong number ");
+        System.out.println(" 22 --> Prime number ");
+        System.out.println(" 23 --> Average ");
+        System.out.println(" 24 --> GCD ");
+        System.out.println(" 25 --> LCM ");
+        System.out.println(" 0 --> Exit ");
+       
+    }
+
+}
