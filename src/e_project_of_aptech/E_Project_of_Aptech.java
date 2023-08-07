e41d48dfd983440163eac14da3a9ff620da617e4
Sharafaldain Al-Sheikh <128252823+Brosfeer@users.noreply.github.com>
2023-08-07 23:23:06 +0300

    Update E_Project_of_Aptech.java

--- src/e_project_of_aptech/E_Project_of_Aptech.java
+++ src/e_project_of_aptech/E_Project_of_Aptech.java
@@ -5,8 +5,8 @@ import java.util.Scanner;
 public class E_Project_of_Aptech extends ListOption {
 
     /**
-     * The main method for running the program. Provides a menu for the user to
-     * choose a mathematical or number-related function.
+     * The main method for running the program. It calls a method that call 
+     * whole Program mwthods for the calculator
      *
      */
     public static void main(String[] args) {
